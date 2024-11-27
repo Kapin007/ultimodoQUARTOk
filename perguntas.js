3c5ea75f17ba9d7efb4a9ece3b35c164e18ad887
Kapin007 <kawasaki.jogos@gmail.com>
2024-11-26 21:30:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Como o Python é utilizado na programação?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o nome da cidade famosa na França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'Por que as funções são importantes em programação?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como você cumprimenta alguém em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
